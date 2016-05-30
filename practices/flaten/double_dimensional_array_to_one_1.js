f391758a7a2d2bd53d0a2300f7c8a7f04c51e79e
wanpanlucky <1462607050@qq.com>
2016-05-30 18:33:46 +0800

    Update double_dimensional_array_to_one_1.js

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,20 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+    var arr=new Array();
+		var Len=collection.length;
+		for(var i=0;i<Len;i++){
+			if(collection[i].length){
+				var innerLen=collection[i].length;
+				for(var j=0;j<innerLen;j++){
+					arr.push(collection[i][j]);
+				}
+			}
+			else{
+				arr.push(collection[i]);	
+			}
+		}
+		return arr;
 }
 
 module.exports = double_to_one;
